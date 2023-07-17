3fcd96a9bf5b0d3913d81a8c6ee9ca1b61510ca3
sidwar76 <75777879+sidwar76@users.noreply.github.com>
2023-07-18 00:14:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# This file contains potential sensitive information
+# such as passwords and API keys.
+
+# Password example
+password = "super_secret_password"
+
+# API key example
+api_key = "your_api_key_here"
+
+# Access key example
+access_key = "your_access_key_here"
+
+# Private key example
+private_key = "your_private_key_here"
